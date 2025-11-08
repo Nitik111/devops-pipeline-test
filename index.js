c40ed37c058817826e1204cd39bdf90420536138
SALLU <nayyarnitik.123@gmail.com>
2025-11-09 01:59:14 +0530

    feat:add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('I am learning Devops');
+console.log('I am learning Ci/Cd pipeline using Github actions');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Start testing');
+setTimeout(() =>console.log('wating for 2 seconds...'),2000) ;
+console.log('Testing complete');
